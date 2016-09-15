b6e1b639c7857801ff4f6897f8943e6704086f0f
Kathleen Sucipto <sucipto.kathleen@gmail.com>
2016-09-15 16:04:05 +0800

    Modify nav content

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+  /* * * * * * * * * * * * * * *
+   * Toggle nav menu content
+   * * * * * * * * * * * * * * */
+  $('#nav-menu-image').on("click", function() {
+    $('#nav-menu-content').toggle();
+  });
+
+  $('#nav-cross').on("click", function(){
+    $('#nav-menu-content').toggle();
+  });
+});
