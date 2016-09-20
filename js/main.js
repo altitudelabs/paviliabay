5d75f148b26eb11f2e1d464a14672236b5dd1de6
Kathleen Sucipto <sucipto.kathleen@gmail.com>
2016-09-20 15:49:45 +0800

    Remove hover function

--- js/main.js
+++ js/main.js
@@ -10,24 +10,25 @@ $(document).ready(function(){
 
   $('#nav-cross').on("click", function(){
     $('#nav-menu-content').toggle();
+    $('#nav-menu-content li').children('ul').css('display', 'none');
   });
 
   /* * * * * * * * * * * * * * *
    * Bind hover so that it's touch friendly
    *  on mobile device
    * * * * * * * * * * * * * * */
-  $('.hover').bind('touchstart touchend', function(e) {
-    e.preventDefault;
-    $(this).toggleClass('hover_effect');
-  });
-
-  // $('#nav-menu-content li').on('click', function(){
-  //   if ($(this).children('ul').css('display') == 'block') {
-  //     $(this).children('ul').css('display', 'none');
-  //   }
-  //   else {
-  //     $(this).children('ul').css('display', 'none');
-  //   }
+  // $('.hover').bind('touchstart touchend', function(e) {
+  //   e.preventDefault;
+  //   $(this).toggleClass('hover_effect');
   // });
+
+  $('#nav-menu-content li a').on('click', function(){
+    if ($(this).parent().children('ul').css('display') == 'block') {
+      $(this).parent().children('ul').css('display', 'none');
+    }
+    else {
+      $(this).parent().children('ul').css('display', 'block');
+    }
+  });
 
 });
