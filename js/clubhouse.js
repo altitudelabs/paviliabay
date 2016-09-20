c0bf2427a985e9e295b0564a5b560267637c5d68
shawncmu <shawn@altitudelabs.com>
2016-09-20 14:25:45 +0800

    displays modal image

--- js/clubhouse.js
+++ js/clubhouse.js
@@ -29,10 +29,59 @@ $window.on('scroll resize', check_if_in_view);
 $window.trigger('scroll');
 
 $('.slide-left').on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',   
-    function(e) {
+  function(e) {
 
-    $('.text').addClass('fade');
+  $('.text').addClass('fade');
+  $('.clubhouse-image-container').addClass('fade');
 
-  });
+});
+
+// Get the modal
+var modal = document.getElementById('imageModal');
+
+// Get the image and insert it inside the modal
+var img = $('.image');
+var modalImg = document.getElementById("modalContent");
+img.on('click', function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+});
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on <span> (x), close the modal
+// span.on('click', function() { 
+//   modal.style.display = "none";
+// });
+
+var changeClubhouseContent = function (currentSection) {
+  if (currentSection == 'clubhouse') {
+  	$('.clubTitle').html('the <br> facade');
+  	$('.clubText').text('TESTING');
+  	$('.clubImage').attr('src','images/clubhouse-section/6_facade1.jpg');
+  	$('.clubLink').text('The Interior');
+    $('#clubhouse-section').data('id', 'facade')
+  } else if (currentSection == 'facade') {
+    $('.clubTitle').html('the <br> interior');
+    $('.clubText').text('TESTING2');
+    $('.clubImage').attr('src','images/clubhouse-section/9_interior2.jpg');
+    $('.clubLink').text('The Clubhouse');
+    $('#clubhouse-section').data('id', 'interior')
+  } else {
+    $('.clubTitle').html('the <br> clubhouse');
+    $('.clubText').text('TESTING1');
+    $('.clubImage').attr('src','images/clubhouse-section/club1.jpg');
+    $('.clubLink').text('The Facade');
+    $('#clubhouse-section').data('id', 'clubhouse')   
+  }
+}
+
+$('.section-change').on('click', function() { 
+  $('.animation-element').removeClass('in-view');
+  $('.animation-element').removeClass('fade');
+  changeClubhouseContent($('#clubhouse-section').data('id'));
+  $window.trigger('scroll');
+});
 
 });
