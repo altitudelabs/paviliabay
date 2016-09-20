4e82d3bfc084d42563bbe8eafa0cf8c4d1fcd74e
Kathleen Sucipto <sucipto.kathleen@gmail.com>
2016-09-20 13:20:56 +0800

    bind hover with touchstart-touchend

--- js/main.js
+++ js/main.js
@@ -10,4 +10,14 @@ $(document).ready(function(){
   $('#nav-cross').on("click", function(){
     $('#nav-menu-content').toggle();
   });
+
+  /* * * * * * * * * * * * * * *
+   * Bind hover so that it's touch friendly
+   *  on mobile device
+   * * * * * * * * * * * * * * */
+  $('.hover').bind('touchstart touchend', function(e) {
+    e.preventDefault();
+    $(this).toggleClass('hover_effect');
+  })
+
 });
