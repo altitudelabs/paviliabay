182ae1625e3101063f85b365e13cf875eeab4140
Kathleen Sucipto <sucipto.kathleen@gmail.com>
2016-09-26 18:27:52 +0800

    initial

--- js/main.js
+++ js/main.js
@@ -31,7 +31,44 @@ $(document).ready(function(){
     }
   });
 
-  // TODO: Mute / unmute video
+
+  /* * * * * * * * * * * * * * *
+  * Video
+  * * * * * * * * * * * * * * */
+  // 
+  // var video = $('video');
+  //
+  // $('#hero-section .wheelbutton-container').on("click", function() {
+  //   console.log("hero-wheelbutton clicked");
+  //   video.css('display', 'block');
+  //   video.get(0).play();
+  //   $('#cover-image').css('display', 'none');
+  //   $('#hero-section .title-container').addClass('one-fifth-opacity');
+  //   $(this).css('visibility', 'hidden');
+  //
+  //   $('.hero-logo').css('visibility', 'hidden');
+  //   $('#nav-menu').css('visibility', 'hidden');
+  // });
+  //
+  // video.on('ended', function(){
+  //   $('video').css('display', 'none');
+  //   $('#cover-image').css('display', 'block');
+  //   $('#hero-body-text').css('visibility', 'visible');
+  //   $('#hero-section .title-container').css('visibility', 'hidden');
+  //   $('.hero-logo').css('visibility', 'visible');
+  //   $('#nav-menu').css('visibility', 'visible');
+  //   $('#hero-section .wheelbutton-container').css('visibility', 'visible');
+  // });
+
+  $('#hero-section .wheelbutton-container').on("click", function() {
+    $('#hero-body-text').css('visibility', 'visible');
+    $('#hero-section .title-container').css('visibility', 'hidden');
+    $('.hero-logo').css('visibility', 'visible');
+    $('#nav-menu').css('visibility', 'visible');
+    $('#hero-section .wheelbutton-container').css('visibility', 'visible');
+  });
+
+  // TODO: Mute / unmute audio
   $('#sound').on('click', function(){
     if ($(this).hasClass('sound-off')){
       $(this).addClass('sound-on').removeClass('sound-off');
